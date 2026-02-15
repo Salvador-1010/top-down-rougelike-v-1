104c777ddb66c6b7a23bd64a121bc90ada28e3e6
Salvador Cruz <salvador10@ucla.edu>
2026-02-14 16:52:31 -0800

    back to working on the tilemaps

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,12 +1,16 @@
 extends CharacterBody2D
 
-@export var speed: float = 120.0
+@export var speed := 125.0
+@export var accel := 1500.0
+@export var friction := 1000.0
 
-func _physics_process(_delta: float) -> void:
-	# Get input as a 2D vector (WASD / arrows)
-	var input_dir := Input.get_vector("move_left", "move_right", "move_up", "move_down")
+func _physics_process(delta):
+	var input_dir := Input.get_vector("move_left","move_right","move_up","move_down")
+	var target_vel := input_dir * speed
 
-	# Normalize so diagonals aren't faster
-	velocity = input_dir * speed
+	if input_dir != Vector2.ZERO:
+		velocity = velocity.move_toward(target_vel, accel * delta)
+	else:
+		velocity = velocity.move_toward(Vector2.ZERO, friction * delta)
 
 	move_and_slide()
