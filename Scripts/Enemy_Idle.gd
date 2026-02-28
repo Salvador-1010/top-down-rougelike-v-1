021eb62363d36560467031cbc8fd15c25acc9296
Salvador Cruz <salvador10@ucla.edu>
2026-02-27 16:21:41 -0800

    Working on enemy idle and puruse state
    added a signal to return the body that entered the pursuit radius (always has to be the player) and now i wanna try to get the idle code to make the enemy wander if the player is not detected.

--- Scripts/Enemy_Idle.gd
+++ Scripts/Enemy_Idle.gd
@@ -2,18 +2,22 @@ extends State
 class_name EnemyIdleState
 
 @export var seek_radius := 30
+var playerTracker : Node2D
+
 
 func enter() -> void:
 	entity.sprite.play("Idle")
+	entity.pursueRadius.get_node("CollisionShape2D").shape.radius = seek_radius
 
 func exit() -> void:
 	pass
 
 func update(_delta : float) -> void:
-	pass
+	print(playerTracker)
 
 func physics_update(_delta: float) -> void:
 	pass
 
-func _on_slime_pursue_radius_area_entered(area: Area2D) -> void:
-	pass # Replace with function body.
+func _on_slime_pursue_radius_body_entered(body: Node2D) -> void:
+	if body.is_in_group("Player"):
+		playerTracker = body
