043aa9d55c03de5af44b8b545ba1da127cf0b72f
Salvador Cruz <salvador10@ucla.edu>
2026-03-03 09:59:37 -0800

    Restarted the enemy wander state
    i restarted bc i got lost in the sauce and thoughht it wouldve jusdt been better to startt from the beginning and lowk its been going super well.
    update: i got the implementation for the code to find a rand_vector inside the seek_radius of the enemy AND i prepared code to store/work with the ORIGINAL original of the enemey to serve as a waypoint they cant move away from
    
    Note: i need to figure out how to convert the rand_vector to a global vector before applying it to the enemy movement
    note: i also dk yet if i should make the wander animation/logic into its own state or not yet unfortunately

--- Scripts/Enemy_Idle.gd
+++ Scripts/Enemy_Idle.gd
@@ -1,49 +1,63 @@
 extends State
 class_name EnemyIdleState
 
-@export var seek_radius := 60.0
+@export var seek_radius := 1.0
 @export var max_attempts := 12
 @export var wait_time := 0.4
 
+var is_wandering : bool
+var destination_picked : bool
+
+#gets the intial global positon to set it as a waypoint so that the calculated rand_vector offset it from the 
+#original global origin rather than the origin after already wandering 
+#(im doing this to prevent the enemy from wandering too far off from the area they were initially placed/spawned in
+var initial_pos 
+
 func enter() -> void:
 	entity.sprite.play("Idle")
-	entity.wander_wait_timer.start(wait_time)
+	#only ever sets the initial_pos once when the idle state is first called
+	if initial_pos == null:
+		initial_pos = entity.global_position
+	#sets the pursue radius to whatever was set in the inspector 
+	entity.pursueRadius.get_node("CollisionShape2D").shape.radius = seek_radius
+	#set is_wandering to false by default
+	is_wandering = false
+	#sets the destination tracker to false by default
+	destination_picked = false
+	
+	#sets the wandering wait timer to whatever was set in the inspector for idle state and then starts the timer
+	entity.wander_wait_timer.wait_time = wait_time
+	entity.wander_wait_timer.start()
 
+func exit() -> void:
+	pass
+
+func update(_delta : float) -> void:
+	entity.global_position += Vector2.RIGHT
+	entity.move_and_slide()
+	print(initial_pos, " ", entity.global_position)
+	#checks if the enemy is supposed to be wandering
+	#also checks that the wander wait timer is stopped (ie the player is on the move)
+	#finally makes sure that the _get_rand_vector() function hasnt already ran 
+	#^ to avoid pikcing more vectors while moving to the first rand vector returned
+	#there probably is a better way to check for the last one but its ok i think it gets the job done
+	if is_wandering and entity.wander_wait_timer.is_stopped() and !destination_picked:
+		print(_get_rand_vector())
 func physics_update(_delta: float) -> void:
-	# wait first (optional)
-	if not entity.wander_wait_timer.is_stopped():
-		return
+	pass
 
-	# pick a valid target
-	var target := _pick_valid_wander_target()
-	if target != null:
-		entity.wander_target_global = target
-		Transitioned.emit(self, "Wander")
-	else:
-		# couldn't find a target, just wait again
-		entity.wander_wait_timer.start(wait_time)
+func _on_wander_wait_timer_timeout() -> void:
+	#sets the next wait length to a rand float 
+	entity.wander_wait_timer.wait_time = randf_range(2.0, 3.5)
+	#stops the timer so that it will not begin counting down again until the rand wander destination is met
+	entity.wander_wait_timer.stop()
+	is_wandering = true
+	destination_picked = false
 
-func _pick_valid_wander_target() -> Variant:
-	for i in range(max_attempts):
-		var offset = _random_point_in_circle(seek_radius)
-		var candidate_global = entity.global_position + offset
 
-		if _path_blocked(entity.global_position, candidate_global):
-			continue
-
-		return candidate_global
-
-	return null
-
-func _random_point_in_circle(r: float) -> Vector2:
-	var ang = randf() * TAU
-	var rad = sqrt(randf()) * r
-	return Vector2(cos(ang), sin(ang)) * rad
-
-func _path_blocked(from_global: Vector2, to_global: Vector2) -> bool:
-	# RayCast2D expects local target from its own origin
-	var rc: RayCast2D = entity.wander_raycast
-	rc.global_position = from_global
-	rc.target_position = rc.to_local(to_global)
-	rc.force_raycast_update()
-	return rc.is_colliding()
+func _get_rand_vector() -> Vector2:
+	var rand_angle = randf() * TAU
+	var rand_radius = sqrt(randf()) * seek_radius
+	var rand_vector = Vector2(cos(rand_angle), sin(rand_angle)) * rand_radius
+	destination_picked = true
+	return rand_vector
--- Scripts/slime.gd
+++ Scripts/slime.gd
@@ -3,4 +3,8 @@ extends CharacterBody2D
 @onready var sprite: AnimatedSprite2D = $AnimatedSprite2D
 @onready var collision: CollisionShape2D = $CollisionShape2D
 @onready var pursueRadius: Area2D = $SlimePursueRadius
-var wander_target_globa: Vector2 = Vector2.ZERO
+@onready var wander_wait_timer: Timer = $WanderWaitTimer
+@onready var wander_raycast: RayCast2D = $WanderRaycast
+
+var wander_target_global: Vector2 = Vector2.ZERO
+var enemy_speed = 125
