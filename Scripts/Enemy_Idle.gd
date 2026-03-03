9630a6e61026b5ee7c8fd3e4d8dfeb03e728460c
Salvador Cruz <salvador10@ucla.edu>
2026-03-02 22:12:33 -0800

    Couldnt get much done but still figuring out the enemy states

--- Scripts/Enemy_Idle.gd
+++ Scripts/Enemy_Idle.gd
@@ -1,41 +1,49 @@
 extends State
 class_name EnemyIdleState
 
-@export var seek_radius := 30
-var playerTracker : Node2D
-var rand_vector 
-var rand_radius
-var rand_angle
+@export var seek_radius := 60.0
+@export var max_attempts := 12
+@export var wait_time := 0.4
 
 func enter() -> void:
 	entity.sprite.play("Idle")
-	entity.pursueRadius.get_node("CollisionShape2D").shape.radius = seek_radius
-	_create_vector()
-
-func exit() -> void:
-	pass
-
-func update(_delta : float) -> void:
-	print(entity.wander_raycast.is_colliding())
+	entity.wander_wait_timer.start(wait_time)
 
 func physics_update(_delta: float) -> void:
-	if entity.wander_wait_timer.is_stopped():
-		pass
+	# wait first (optional)
+	if not entity.wander_wait_timer.is_stopped():
+		return
 
-func _on_slime_pursue_radius_body_entered(body: Node2D) -> void:
-	if body.is_in_group("Player"):
-		playerTracker = body
+	# pick a valid target
+	var target := _pick_valid_wander_target()
+	if target != null:
+		entity.wander_target_global = target
+		Transitioned.emit(self, "Wander")
+	else:
+		# couldn't find a target, just wait again
+		entity.wander_wait_timer.start(wait_time)
 
-func _create_vector() -> void:
-	rand_angle = randf() * TAU
-	rand_radius = sqrt(randf()) * seek_radius
-	rand_vector = Vector2(cos(rand_angle), sin(rand_angle)) * rand_radius
-	entity.wander_raycast.target_position = rand_vector
-	if entity.wander_raycast.is_colliding():
-		print("reran")
-		_create_vector()
-	
+func _pick_valid_wander_target() -> Variant:
+	for i in range(max_attempts):
+		var offset = _random_point_in_circle(seek_radius)
+		var candidate_global = entity.global_position + offset
 
+		if _path_blocked(entity.global_position, candidate_global):
+			continue
 
-func _on_wander_wait_timer_timeout() -> void:
-	print('hi')
+		return candidate_global
+
+	return null
+
+func _random_point_in_circle(r: float) -> Vector2:
+	var ang = randf() * TAU
+	var rad = sqrt(randf()) * r
+	return Vector2(cos(ang), sin(ang)) * rad
+
+func _path_blocked(from_global: Vector2, to_global: Vector2) -> bool:
+	# RayCast2D expects local target from its own origin
+	var rc: RayCast2D = entity.wander_raycast
+	rc.global_position = from_global
+	rc.target_position = rc.to_local(to_global)
+	rc.force_raycast_update()
+	return rc.is_colliding()
--- Scripts/slime.gd
+++ Scripts/slime.gd
@@ -3,5 +3,4 @@ extends CharacterBody2D
 @onready var sprite: AnimatedSprite2D = $AnimatedSprite2D
 @onready var collision: CollisionShape2D = $CollisionShape2D
 @onready var pursueRadius: Area2D = $SlimePursueRadius
-@onready var wander_raycast: RayCast2D = $WanderRaycast
-@onready var wander_wait_timer: Timer = $WanderWaitTimer
+var wander_target_globa: Vector2 = Vector2.ZERO
